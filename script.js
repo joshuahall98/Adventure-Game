be6d6e423a305952c671b5622a6692f17523dc80
Cheesemcpuff <joshuahall98@live.com>
2020-02-26 16:31:41 +0000

    new js and other edits

--- script.js
+++ script.js
@@ -35,6 +35,8 @@ function isInputNumber(evt){
 // var ww;
 // var www;
 
+
+
 function setAttack(){
 	var x = parseInt(document.getElementById("PATK").value);
 	var xx = 6 + x;
@@ -45,9 +47,6 @@ function setAttack(){
 
 }
 
-
-
- 
 
 
 function setHealth(){
@@ -99,39 +98,4 @@ window.onload=stats;
 
 
 
-function npdata(){
-	var np = localStorage.getItem("Edit");
-	document.getElementById("writing").innerHTML = np;
 
-	
-}
-window.onload=npdata;
-
-function npupdate(){
-	// Get the existing data
-	var existing = localStorage.getItem('Edit');
-
-	var npup = document.getElementById("npad").value;
-
-	// If no existing data, use the value by itself
-	// Otherwise, add the new value to it
-	var data = existing ? existing + (' /// ')+npup : '';
-
-	// Save back to localStorage
-	localStorage.setItem('Edit', data);
-
-	var nppup = localStorage.getItem("Edit");
-
-	document.getElementById("writing").innerHTML = (nppup)
-}
-
-function resetForm(){
-	document.getElementById("npadForm").reset();
-}
-
-
-function newgame(){
-	localStorage.clear();
-	var empty = " ";
-	localStorage.setItem("Edit", empty);
-}
